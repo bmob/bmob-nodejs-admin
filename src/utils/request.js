d4a11e5e52e54d09ad80e9dc8c961c131920c54d
magic <ysjheeqg@163.com>
2018-02-07 14:14:14 +0800

    fix bug

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { Message, MessageBox } from 'element-ui'
 import store from '../store'
-// import { getToken } from '@/utils/auth'
+import { getToken } from '@/utils/auth'
 
 // 创建axios实例
 const service = axios.create({
--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,17 +1,25 @@
 <template>
     <div class="app-container">
-        <el-table :data="tableData" border style="width: 100%">
-            <el-table-column fixed prop="date" label="日期" width="150">
+
+        <!-- Table -->
+        <el-button type="text" @click="dialogTableVisible = true">打开嵌套表格的 Dialog</el-button>
+
+        <el-dialog title="详细信息" :visible.sync="dialogTableVisible">
+          <span>这是一段信息</span>
+        </el-dialog>
+
+        <el-table :data="tableData" v-loading.body="listLoading" border style="width: 100%">
+            <el-table-column fixed prop="objectId" label="id" width="150">
             </el-table-column>
-            <el-table-column prop="name" label="姓名" width="120">
+            <el-table-column prop="username" label="用户名" width="120">
             </el-table-column>
             <el-table-column prop="province" label="省份" width="120">
             </el-table-column>
-            <el-table-column prop="city" label="市区" width="120">
+            <el-table-column prop="openid" label="openid" width="300">
             </el-table-column>
-            <el-table-column prop="address" label="地址" width="300">
-            </el-table-column>
-            <el-table-column prop="zip" label="邮编" width="120">
+            <el-table-column prop="updatedAt" label="更新时间" width="300">
+                </el-table-column>
+            <el-table-column prop="createdAt" label="加入时间" width="120">
             </el-table-column>
             <el-table-column fixed="right" label="操作" width="100">
                 <template slot-scope="scope">
@@ -24,6 +32,9 @@
         <el-pagination background layout="prev, pager, next" @current-change="handleCurrentChange" @size-change="handleSizeChange"
             :total="1000">
         </el-pagination>
+
+
+
     </div>
 </template>
 
@@ -33,6 +44,7 @@
         methods: {
             handleClick(row) {
                 console.log(row);
+                this.dialogTableVisible=true;
             },
             handleSizeChange(val) {
                 this.pagesize = val;
@@ -45,8 +57,8 @@
             fetchData() {
                 this.listLoading = true
                 getList(this.listQuery).then(response => {
-                    console.log(response.results);
-                    this.list = response.results
+                    console.log(response.data.results);
+                    this.tableData = response.data.results
                     this.listLoading = false
                 })
             },
@@ -61,14 +73,26 @@
 
         data() {
             return {
-                tableData: [{
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }]
+                gridData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }],
+        dialogTableVisible: false,
+        formLabelWidth: '120px',
+                tableData: []
             }
         }
     }
